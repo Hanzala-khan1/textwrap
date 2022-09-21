9ca6656f9218a6490accf73216dd3b3c22d1d565
hunny-nik <106017090+hunny-nik@users.noreply.github.com>
2022-09-21 03:41:39 -0700

    Updates

---
+++ precache-manifest.ccf1dc8d6eb5936c7b1c8fa983eaa21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3cf5430e1f5d7c3758b",
+    "url": "/textwrap/static/css/main.6792b6a0.chunk.css"
+  },
+  {
+    "revision": "f3cf5430e1f5d7c3758b",
+    "url": "/textwrap/static/js/main.f3cf5430.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/textwrap/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "d41b552e711c7f08f5ef",
+    "url": "/textwrap/static/js/2.d41b552e.chunk.js"
+  },
+  {
+    "revision": "ab415c185c4c2a620546",
+    "url": "/textwrap/static/js/runtime~main.ab415c18.js"
+  },
+  {
+    "revision": "ad08b71b83b0c6569f03f16af049f30b",
+    "url": "/textwrap/index.html"
+  }
+];
